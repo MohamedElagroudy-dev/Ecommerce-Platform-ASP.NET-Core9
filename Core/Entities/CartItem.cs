97b2901e69d08b3721a2026a0fb48addd17d499a
MohamedElagroudy-dev <Mohamed.Elagroudy.dev@gmail.com>
2025-09-14 22:52:00 +0300

    Add Cartservice & config

--- Core/Entities/CartItem.cs
+++ Core/Entities/CartItem.cs
@@ -12,8 +12,6 @@ namespace Core.Entities
         public required string ProductName { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
-        public required string PictureUrl { get; set; }
-        public required string Brand { get; set; }
-        public required string Type { get; set; }
+        public List<string> PictureUrls { get; set; } = new();
     }
 }
