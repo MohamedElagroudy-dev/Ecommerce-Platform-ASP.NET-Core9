637b546096faec1e4e2f453041de290f436447e5
MohamedElagroudy-dev <Mohamed.Elagroudy.dev@gmail.com>
2025-09-15 23:44:22 +0300

    Seed Roles

--- Infrastructure/Config/RoleConfiguration.cs
+++ Infrastructure/Config/RoleConfiguration.cs
@@ -1,22 +1,23 @@
-﻿//using Microsoft.AspNetCore.Identity;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.EntityFrameworkCore.Metadata.Builders;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace Infrastructure.Config
-//{
-//    public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
-//    {
-//        public void Configure(EntityTypeBuilder<IdentityRole> builder)
-//        {
-//            builder.HasData(
-//            new IdentityRole { Id = "admin-id", Name = "Admin", NormalizedName = "ADMIN" },
-//            new IdentityRole { Id = "customer-id", Name = "Customer", NormalizedName = "CUSTOMER" }
-//        );
-//        }
-//    }
-//}
+namespace Infrastructure.Config
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+            new IdentityRole { Id = "admin-id", Name = "Admin", NormalizedName = "ADMIN" },
+            new IdentityRole { Id = "customer-id", Name = "Customer", NormalizedName = "CUSTOMER" },
+            new IdentityRole { Id = "Siller-id", Name = "Siller", NormalizedName = "SILLER" }
+        );
+        }
+    }
+}
