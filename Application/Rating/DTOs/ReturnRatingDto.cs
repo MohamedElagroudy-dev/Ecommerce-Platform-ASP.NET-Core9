2f107ac021eb25f434a8add1b08fe3bf35fa204f
MohamedElagroudy-dev <Mohamed.Elagroudy.dev@gmail.com>
2025-10-05 19:59:38 +0300

    Add Rating Table and Dtos

---
+++ Application/Rating/DTOs/ReturnRatingDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Rating.DTOs
+{
+    public class ReturnRatingDto
+    {
+        public int Stars { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public DateTime ReviewTime { get; set; }
+    }
+}
